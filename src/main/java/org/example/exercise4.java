278e6556b36842af08fd142e1804b8054d5294d5
Emily Gensch <egensch@knights.ucf.edu>
2021-06-06 17:17:05 -0400

    Initial commit

---
+++ src/main/java/org/example/exercise4.java
@@ -0,0 +1,22 @@
+package org.example;
+import java.util.Scanner;
+
+public class exercise4 {
+    public static void main(String[] args) {
+        Scanner inputQuote = new Scanner(System.in);
+
+        System.out.print("Enter a noun: ");
+        String noun = inputQuote.nextLine();
+
+        System.out.print("Enter a verb: ");
+        String verb = inputQuote.nextLine();
+
+        System.out.print("Enter an adjective: ");
+        String adjective = inputQuote.nextLine();
+
+        System.out.print("Enter an adverb: ");
+        String adverb = inputQuote.nextLine();
+
+        System.out.println("Do you " + verb + " your " + adjective + " " + noun + " " + adverb + "? That's hilarious!");
+    }
+}
